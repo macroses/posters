32c486f1dc147adb3f0813f9e094a73a3c625eec
Лщтыефтешт <>
2021-04-06 23:11:44 +0300

    Доделал сортировку по "избранным".

--- src/components/main/Main.svelte
+++ src/components/main/Main.svelte
@@ -3,22 +3,45 @@
     import PostList from '../posts/PostList.svelte';
     
     let allPosts = posts;
+    let activeButton = 1;
+    let visibleFavorite = false;
     
 
     function sortByPrice () {
         allPosts = allPosts.sort((a, b) => parseInt(a.itemPrice) > parseInt(b.itemPrice) ? 1 : -1);
+        activeButton = 2;
     }
 
     function sortByViews () {
         allPosts = allPosts.sort((a, b) => parseInt(a.viewsCount) > parseInt(b.viewsCount) ? -1 : 1);
+        activeButton = 1;
     }
 
     function sortByDate () {
         allPosts = allPosts.sort((a, b) => {
             let c = new Date(a.itemPostDate).getTime();
             let d = new Date(b.itemPostDate).getTime();
-            return c > d ? 1 : -1;
+            return c > d ? -1 : 1;
         });
+        activeButton = 3;
+    }
+
+    function sortByFavorite () {
+        visibleFavorite = !visibleFavorite;
+
+        if (visibleFavorite) {
+            allPosts = allPosts.filter((item) => {
+                return item.itemFavorite == true;
+            })
+            
+            if (allPosts.length == 0) {
+                alert("добавьте что-нибудь в избранное")
+                visibleFavorite = false;
+                return allPosts = posts;
+            }
+        } else {
+            allPosts = posts;
+        }
     }
 
 </script>
@@ -29,12 +52,12 @@
     <div class="descr">Показать сначала:</div>
     <div class="content_nav">
         <div class="btn_group">
-            <button on:click={sortByViews}  class="active">Популярные</button>
-            <button on:click={sortByPrice}>Дешевые</button>
-            <button on:click={sortByDate}>Новые</button>
+            <button on:click={sortByViews} class:active={activeButton === 1}>Популярные</button>
+            <button on:click={sortByPrice} class:active={activeButton === 2}>Дешевые</button>
+            <button on:click={sortByDate} class:active={activeButton === 3}>Новые</button>
         </div>
         <div class="favorite_btn">
-            <button>
+            <button on:click={sortByFavorite} class:active={visibleFavorite}>
                 <span class="material-icons">
                     favorite_border
                     </span>
@@ -42,8 +65,13 @@
             </button>
         </div>
     </div>
+    {#await allPosts then value}
+        <PostList allPostsArr={value}></PostList>
+
+        
+    {/await}
     
-    <PostList allPostsArr={allPosts}></PostList>
+    
 </main>
 
 <style lang=scss> 
@@ -70,15 +98,21 @@
         margin-bottom: 20px;
         display: flex;
 
+        
+    }
+
+    .btn_group {
+        display: flex;
         button {
             border: 1px solid lightgray;
             padding: 10px;
             background: rgba(0,0,0,.0);
-            margin-left: -1px;
+            margin-left: -2px;
             display: flex;
             align-items: center;
             &.active {
                 background: var(--bg-color);
+                border: 1px solid var(--bg-color);
                 color: #fff;
             }
 
@@ -95,12 +129,25 @@
         }
     }
 
-    .btn_group {
-        display: flex;
-    }
-
     .favorite_btn {
         margin-left: auto;
+
+        button {
+            border: 1px solid lightgray;
+            padding: 10px;
+            background: rgba(0,0,0,.0);
+            display: flex;
+            align-items: center;
+            border-radius: 6px;
+            span {
+                margin-right: 5px;
+            }
+            &.active {
+                background: var(--bg-color);
+                border: 1px solid var(--bg-color);
+                color: #fff;
+            }
+        }
     }
 
 </style>
